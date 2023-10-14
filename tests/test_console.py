3ace4d4da189287de09d6511fbacae2fdf96e0b6
poquitodev <122402632+poquitodev@users.noreply.github.com>
2023-10-14 19:26:52 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -89,7 +89,7 @@ class TestHBNBCommand_help(unittest.TestCase):
             self.assertEqual(h, output.getvalue().strip())
 
     def test_help_EOF(self):
-        h = "EOF signal to exit the program"
+        h = "EOF signal to exit the program."
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help EOF"))
             self.assertEqual(h, output.getvalue().strip())
